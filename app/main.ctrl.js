6fc21778d1449b8493d8c3ad66c4438fb585c6bf
Vincent Colavin & Vincent Colavin <pair+vcolavin.vcolavin@devbootcamp.com>
2015-01-25 14:50:16 -0800

    initial commit

---
+++ app/main.ctrl.js
@@ -0,0 +1,55 @@
+var myModule = angular.module('app', [])
+
+myModule.controller("MainController", ['$scope', '$http', function($scope, $http){
+
+//Initial page load get users
+
+    // $http.get("http://nomadr-api.herokuapp.com/api/users").success(function(response){
+    //     console.log(response)
+    //     $scope.users = response
+    // }).error(function(){
+    //     $scope.quote =  "Request Failed!"
+    // })
+    $scope.bgImg = 'http://www.mrwallpaper.com/wallpapers/Paris-City-Lights.jpg';
+
+
+    $scope.loggedInUser = "54c401f3a9a17b88f9000002"
+
+// Get current user
+
+    $http.get("http://nomadr-api.herokuapp.com/api/users/"+$scope.loggedInUser).success(function(response){
+
+        $scope.currentUser = response
+
+// Trying to turn city into query string so I can plug it into URL!!!! (can I use JQUERY here?)
+
+
+        $http.get('http://api.openweathermap.org/data/2.5/weather?q='+$scope.currentUser.city).success(function(response){
+            $scope.weather = response
+        })
+
+    }).error(function(){
+        $scope.quote =  "Request Failed!"
+    })
+
+//Change these GETs after change from localhost?
+
+//Get
+    // $http.get('http://api.openweathermap.org/data/2.5/weather?q=disneyland').success(function(response){
+    //     console.log(response)
+    //     $scope.weather = response
+    // })
+
+    $scope.farenheit = function(kelvin) {
+        var num = 1.8 * (kelvin - 273) + 32
+        return num.toFixed()
+    }
+
+//Page title
+    $scope.title = "Nomadr";
+
+    $scope.teammembers = "Vincent Colavin, Valerie Smith, Philip Riley, Devin Liu, Alfred Calayag";
+
+    $scope.searchInput = '';
+
+}]);
---
+++ server.js
@@ -0,0 +1,49 @@
+
+// BASE SETUP
+// =============================================================================
+
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+router.use(function(request, response, next){
+  console.log('Something is happening')
+  next();
+})
+
+app.use(express.static(__dirname + '/app'));
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/', function(request, response) {
+//     console.log('hey');
+//     response.render('app/views/index.html');
+// });
+
+// User routes
+// -------------------
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+// app.use('/client', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
