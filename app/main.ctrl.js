f7cba448f0dd4e1bf2f2fd3999fbad24a349212c
Alfred Calayag & Valerie Smith <pair+alfredcalayag.valeriesmith@devbootcamp.com>
2015-01-26 13:51:52 -0800

    Photo background working

--- app/main.ctrl.js
+++ app/main.ctrl.js
@@ -2,23 +2,20 @@ var myModule = angular.module('app', [])
 
 myModule.controller("MainController", ['$scope', '$http', function($scope, $http){
 
+    $scope.loggedInUser = "54c401f3a9a17b88f9000002"
 
-    $scope.bgImg = 'http://www.mrwallpaper.com/wallpapers/Paris-City-Lights.jpg';
+    $http.get('http://localhost:9090/api/google_photo/'+$scope.loggedInUser).success(function(response){
+        console.log(response.photos.length)
+        var maxNum = response.photos.length
+        var randNum = (Math.floor(Math.random()*(maxNum - 0) + 0))
+        $scope.bgImg = response.photos[randNum]
+    })
+    // $scope.bgImg = 'http://www.mrwallpaper.com/wallpapers/Paris-City-Lights.jpg';
 
 // set this http get to user:
-    $http({
-        url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
-        method: 'GET',
-        headers: {},
-        params: {
-            location: '-33.8670522,151.1957362',
-            radius: '5000',
-            key: '' //PUT IN ENV FILE
-        }
-    })
 
 
-    $scope.loggedInUser = "54c401f3a9a17b88f9000002"
+
 
 // Get current user
 
