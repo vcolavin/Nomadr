e43eccd4f769b58e2d0a58d5a63e5e139e4709e6
Alfred Calayag & Valerie Smith <pair+alfredcalayag.valeriesmith@devbootcamp.com>
2015-01-27 17:24:25 -0800

    Fixed Welcome and Logo and form placement for signup page

--- public/controllers/splash.ctrl.js
+++ public/controllers/splash.ctrl.js
@@ -1,5 +1,5 @@
 ourApp.controller("SplashController", ['$scope', '$http', function($scope, $http){
-  $scope.title = "Nomadr"
+  $scope.title = "NomadR"
   $scope.background = "http://static.pexels.com/wp-content/uploads/2015/01/clouds-flying-high-4103.jpeg"
   console.log("hey hey hey hey")
 }]);
