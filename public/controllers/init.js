08f491e65c6535e1fbaff757500848cd7783b9b6
devin-liu & Vincent Colavin <pair+devin-liu.vcolavin@devbootcamp.com>
2015-01-28 17:19:54 -0800

    works in chrome

--- public/controllers/init.js
+++ public/controllers/init.js
@@ -4,16 +4,16 @@ ourApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/', {
-        templateUrl: '../partials/splash.html'
+        templateUrl: 'partials/splash.html'
       }).
       when('/signin', {
-        templateUrl: '../partials/signin.html'
+        templateUrl: 'partials/signin.html'
       }).
       when('/signup', {
-        templateUrl: '../partials/signup.html'
+        templateUrl: 'partials/signup.html'
       }).
       when('/home', {
-        templateUrl: '../partials/home.html'
+        templateUrl: 'partials/home.html'
       }).
       otherwise({
         redirectTo: '/'
