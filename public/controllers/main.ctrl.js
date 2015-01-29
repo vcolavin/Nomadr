d7a92e53bf8abd603527c38c83f3a9eadc54cfd9
Philip Riley & Vincent Colavin <pair+philril.vcolavin@devbootcamp.com>
2015-01-28 18:18:57 -0800

    Adds regex for wiki. Still needs a little tweaking. Goes to end of sentence in some cases

--- public/controllers/main.ctrl.js
+++ public/controllers/main.ctrl.js
@@ -48,7 +48,8 @@ ourApp.controller("MainController", ['$scope', '$http', '$route','$cookies', '$l
         // FIXME: city names with a space break this
         // Get Wiki Info
         $http.get('http://nomadr-api.herokuapp.com/api/wiki/'+$scope.loggedInUser).success(function(response){
-            $scope.wiki_data = response.wiki_content
+            $scope.wiki_data = response.wiki_content.replace(/\(.*\)/, "")
+            console.log(response.wiki_content)
         }).error(function() {
             console.log("wiki data failed")
         })
