c610d065b6deacae992cb09971c79d5d19b7225d
Valerie Smith & Valerie Smith <pair+valeriesmith.valeriesmith@devbootcamp.com>
2015-01-28 17:51:08 -0800

    remove unnecessary comments

--- public/controllers/main.ctrl.js
+++ public/controllers/main.ctrl.js
@@ -14,29 +14,13 @@ ourApp.controller("MainController", ['$scope', '$http', '$route','$cookies', '$l
         $scope.leaveDate = moment(new Date($scope.currentUser.departureDate)).format("MMMM Do, YYYY")
         $scope.fromNow = moment(new Date($scope.currentUser.departureDate)).fromNow()
 
-        //Get Weather (from weather api, not ours)
-        // $http.get('http://localhost:9090/api/weather/'+$scope.loggedInUser).success(function(response){
-        //     $scope.weather = response.weather
-        //     console.log(response.weather.weather[0].main)
-        //     console.log(weather['weather'][0]['main'])
-        // })
-
         // Get weather
         $http.get('http://nomadr-api.herokuapp.com/api/weather/'+$scope.loggedInUser).success(function(response){
             $scope.weather = response.weather
          })
 
-        //Get GOOGLE photo
-        // $http.get('http://nomadr-api.herokuapp.com/api/google_photo/'+$scope.loggedInUser).success(function(response){
-        //     // console.log(response)
-        //     var maxNum = response.photos.length
-        //     var randNum = (Math.floor(Math.random()*(maxNum - 0) + 0))
-        //     $scope.bgImg = response.photos[randNum]
-        //  })
-
         //Get PANARAMIO photo:
         $http.get('http://nomadr-api.herokuapp.com/api/panaramio/'+$scope.loggedInUser).success(function(response){
-            // console.log(response)
             var maxNum          = response.photos.length
             var randNum         = (Math.floor(Math.random()*(maxNum - 0) + 0))
             $scope.bgImg        = response.photos[randNum].photo_url
@@ -45,7 +29,6 @@ ourApp.controller("MainController", ['$scope', '$http', '$route','$cookies', '$l
             $scope.imgOwnerUrl  = response.photos[randNum].owner_url
          })
 
-        // FIXME: city names with a space break this
         // Get Wiki Info
         $http.get('http://nomadr-api.herokuapp.com/api/wiki/'+$scope.loggedInUser).success(function(response){
             $scope.wiki_data = response.wiki_content
